3c5163cf07f150ad267136fc0ee1fb0a42fe8e15
Mehedi Hasan Arafat <mehedihasanarafat10@gmail.com>
2023-05-10 16:29:48 +0600

    api connected

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
+// import React, { useCallback } from 'react';
+// import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'reactflow';
+//
+// import 'reactflow/dist/style.css';
+//
+// const initialNodes = [
+//   { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+//   { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+// ];
+// const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+//
+// export default function App() {
+//   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+//   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+//
+//   const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+//
+//   return (
+//     <div style={{ width: '100vw', height: '100vh' }}>
+//       <ReactFlow
+//         nodes={nodes}
+//         edges={edges}
+//         onNodesChange={onNodesChange}
+//         onEdgesChange={onEdgesChange}
+//         onConnect={onConnect}
+//       />
+//     </div>
+//   );
+// }
+
+
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        fetch('/api/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(
+                {
+  "name": "John Doe",
+  "age": 30,
+  "email": "johndoe@example.com"
+}
+
+
+            )
+        })
+            .then(response => response.json())
+            .then(data => {
+                setMessage(data.message);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+
+    return (
+        <div className="App">
+            <h1>Send Request to Django API</h1>
+            <form onSubmit={handleSubmit}>
+                <button type="submit">Send Request</button>
+            </form>
+            {message && <p>Response: {message}</p>}
+        </div>
+    );
 }
 
 export default App;
